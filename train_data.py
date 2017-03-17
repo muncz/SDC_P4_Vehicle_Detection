d1a50386cea4c5da87ff823ed26093c14f6a9fa6
Tomasz Dowgielewicz <td@atest.co.uk>
2017-03-17 17:37:06 +0100

    feat: Train data classified

--- train_data.py
+++ train_data.py
@@ -4,13 +4,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from skimage.feature import hog
+import time
+from sklearn.svm import LinearSVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.cross_validation import train_test_split
 
 
 
-#Some global parameter
+#Some macro parameter
 hog_pix_per_cell = 8
 hog_cell_per_block = 2
 hog_orient = 12
+hog_target_size = (64,64)
 
 
 def get_train_images():
@@ -33,6 +38,8 @@ def bin_spatial(img, color_space='RGB', size=(32, 32)):
             feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
         elif color_space == 'YCrCb':
             feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+        elif color_space == 'gray':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     else:
         feature_image = np.copy(img)
     # Use cv2.resize().ravel() to create the feature vector
@@ -66,36 +73,104 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, featu
         return features
 
 
-def get_histogram_features(img):
-    pass
 
 
 
 def get_img_features(colr_img):
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-    orient = 8
-    pix_per_cell = 8
-    cell_per_block = 2
+    features = []
+
+    copy = np.copy(colr_img)
+    copy = cv2.resize(copy,hog_target_size)
+
+    #gray = cv2.cvtColor(copy, cv2.COLOR_RGB2HLS)[:, :, 0]
+    gray = cv2.cvtColor(copy, cv2.COLOR_RGB2GRAY)
+    #ensure every image is same size
+
+    orient = hog_orient
+    pix_per_cell = hog_pix_per_cell
+    cell_per_block = hog_cell_per_block
     # Call our function with vis=True to see an image output
-    hog_features = get_hog_features(gray, orient,
-                                           pix_per_cell, cell_per_block,
-                                           vis=False, feature_vec=False)
+    hog_features = get_hog_features(gray, orient, pix_per_cell, cell_per_block, vis=False, feature_vec=True)
 
-    print(hog_features)
+
+    hls_features = bin_spatial(colr_img,"HLS")
+    #rgb_features = bin_spatial(colr_img)
+    # print(hog_features)
+    # print(len(rgb_features))
+    # print(hog_features)
+
+    return np.concatenate((hls_features,hog_features))
+
+
+
+
+    # hog_features,hog_image = get_hog_features(gray, orient, pix_per_cell, cell_per_block, vis=True, feature_vec=False)
+    # print(hog_features)
     # plt.imshow(hog_image,cmap="gray")
     # plt.show()
-    # mpimg.imsave("report/veh2hog.png",hog_image )
+    # mpimg.imsave("report/x.png",hog_image )
 
 
 
 
-img = mpimg.imread("report/veh2.png")
+def train_classifier():
+
+    car_features = []
+    notcar_features = []
+
+    vehicles_paths, non_vehicles_paths = get_train_images()
+
+    for f in vehicles_paths:
+        img =  mpimg.imread(f)
+        car_features.append(get_img_features(img))
+
+    for f in non_vehicles_paths:
+        img =  mpimg.imread(f)
+        notcar_features.append(get_img_features(img))
+
+    y = np.hstack((np.ones(len(car_features)),
+                   np.zeros(len(notcar_features))))
+
+    X = np.vstack((car_features, notcar_features)).astype(np.float64)
+    # Fit a per-column scaler
+    X_scaler = StandardScaler().fit(X)
+    # Apply the scaler to X
+    scaled_X = X_scaler.transform(X)
+
+    # Split up data into randomized training and test sets
+    rand_state = np.random.randint(0, 100)
+    X_train, X_test, y_train, y_test = train_test_split(
+        scaled_X, y, test_size=0.2, random_state=rand_state)
+
+    #print('Using:', orient, 'orientations', pix_per_cell,          'pixels per cell and', cell_per_block, 'cells per block')
+    print('Feature vector length:', len(X_train[0]))
+    # Use a linear SVC
+    svc = LinearSVC()
+    # Check the training time for the SVC
+    t = time.time()
+    svc.fit(X_train, y_train)
+    t2 = time.time()
+    print(round(t2 - t, 2), 'Seconds to train SVC...')
+    # Check the score of the SVC
+    print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+    # Check the prediction time for a single sample
+    t = time.time()
+    n_predict = 10
+    print('My SVC predicts: ', svc.predict(X_test[0:n_predict]))
+    print('For these', n_predict, 'labels: ', y_test[0:n_predict])
+    t2 = time.time()
+    print(round(t2 - t, 5), 'Seconds to predict', n_predict, 'labels with SVC')
 
 
-get_img_features(img)
+img = mpimg.imread("report/nei4.png")
+
+
+print (get_img_features(img))
 
 
 feature_vec = bin_spatial(img, color_space='RGB', size=(32, 32))
+
+train_classifier()
 
 #compose histogram features with with hog features
