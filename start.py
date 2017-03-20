d99defbe42ef443b9ebdf1171d4e5ebcd562c49e
Tomasz Dowgielewicz <t.dowgielewicz@gmail.com>
2017-03-20 16:34:13 +0100

    feat: Added heat map to reduce False Detection

--- start.py
+++ start.py
@@ -22,6 +22,8 @@ def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, ce
     draw_img = np.copy(img)
     img = img.astype(np.float32) / 255
 
+    boxes = []
+
     img_tosearch = img[ystart:ystop, :, :]
     ctrans_tosearch = cv2.cvtColor(img_tosearch, cv2.COLOR_RGB2HLS)
     if scale != 1:
@@ -79,12 +81,22 @@ def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, ce
                 xbox_left = np.int(xleft * scale)
                 ytop_draw = np.int(ytop * scale)
                 win_draw = np.int(window * scale)
-                cv2.rectangle(draw_img, (xbox_left, ytop_draw + ystart),
-                              (xbox_left + win_draw, ytop_draw + win_draw + ystart), (0, 0, 255), 6)
+                #cv2.rectangle(draw_img, (xbox_left, ytop_draw + ystart), (xbox_left + win_draw, ytop_draw + win_draw + ystart), (0, 0, 255), 6)
+                boxes.append([(xbox_left, ytop_draw + ystart),
+                              (xbox_left + win_draw, ytop_draw + win_draw + ystart)])
 
+    return draw_img, boxes
+
+
+def draw_boxes(img, bboxes, color=(0, 0, 255), thick=3):
+    # Make a copy of the image
+    draw_img = np.copy(img)
+    # Iterate through the bounding boxes
+    for bbox in bboxes:
+        # Draw a rectangle given bbox coordinates
+        cv2.rectangle(draw_img, bbox[0], bbox[1], color, thick)
+    # Return the image copy with boxes drawn
     return draw_img
-
-
 
 
 
@@ -107,10 +119,62 @@ cell_per_block = svc_pickle["cell_per_block"]
 spatial_size = svc_pickle["spatial_size"]
 hist_bins = svc_pickle["hist_bins"]
 
-out_img = find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins)
+out_img,boxes = find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins)
 
-# plt.imshow(out_img)
-# plt.show()
+print(boxes)
+
+
+
+out = draw_boxes(out_img,boxes)
+
+heat = np.zeros_like(out[:,:,0]).astype(np.float)
+
+def add_heat(heatmap, bbox_list):
+    # Iterate through list of bboxes
+    for box in bbox_list:
+        # Add += 1 for all pixels inside each bbox
+        # Assuming each "box" takes the form ((x1, y1), (x2, y2))
+        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+
+    # Return updated heatmap
+    return heatmap# Iterate through list of bboxes
+
+def apply_threshold(heatmap, threshold):
+    # Zero out pixels below the threshold
+    heatmap[heatmap <= threshold] = 0
+    # Return thresholded map
+    return heatmap
+
+
+
+def draw_labeled_bboxes(img, labels):
+    # Iterate through all detected cars
+    for car_number in range(1, labels[1]+1):
+        # Find pixels with each car_number label value
+        nonzero = (labels[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+        # Draw the box on the image
+        cv2.rectangle(img, bbox[0], bbox[1], (0,0,255), 6)
+    # Return the image
+    return img
+
+from scipy.ndimage.measurements import label
+
+heat = add_heat(heat,boxes)
+heat = apply_threshold(heat,3)
+
+heatmap = np.clip(heat, 0, 255)
+
+labels = label(heatmap)
+
+draw_img = draw_labeled_bboxes(np.copy(out_img), labels)
+
+plt.imshow(draw_img)
+plt.show()
 
 frame_id = 0
 while(cap.isOpened()):
